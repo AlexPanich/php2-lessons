8bbe7889da2f86bcf523f8525ef294a5ef12c9da
Albert Stepantsev <philfromfmf@mail.ru>
2016-01-29 21:29:12 +0300

    ! ActiveRecord

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Models\HasEmail;
 use App\Models\User;
 
 require __DIR__ . '/autoload.php';
 
-$users = User::findAll();
-
-function sendEmail(HasEmail $user, string $message)
-{
-    echo 'Почта уходит на ' . $user->email;
-}
-
-sendEmail($users[0], 'Hello!');
+$user = new User();
+$user->name = 'Vasya';
+$user->email = 'v@pupkin.ru';
+$user->insert();
