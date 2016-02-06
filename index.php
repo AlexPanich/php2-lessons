c4bab34d90fb33eaafd3b21ce73fd9cd089e24f4
Albert Stepantsev <test@pr-of-it.ru>
2016-02-06 12:14:10 +0300

    + View

--- index.php
+++ index.php
@@ -8,4 +8,7 @@ $view = new \App\View();
 $view->title = 'Мой крутой сайт!';
 $view->users = \App\Models\User::findAll();
 
+echo count($view);
+die;
+
 $view->display(__DIR__ . '/App/templates/index.php');
